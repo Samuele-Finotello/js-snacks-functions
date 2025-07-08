20937da0995ce1cf7ff4a445d43812fde0aa092c
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-08 19:02:31 +0200

    [add] feat esercizio 4: declared array vocaliPresenti and for

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,12 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-const vocali = (vocali,) => {
-  vocali = 0;
+const vocali = (contaVocali, vocaliPresenti) => {
+  contaVocali = 0;
+  vocaliPresenti = [];
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === 'a' || word[i] === 'e' || word[i] === 'i' || word[i] === 'o' || word[i] === 'u') {
+      contaVocali++;
+      vocaliPresenti.push(word[i]);
+    }
+  }
+  vocaliPresenti.toString();
+  return contaVocali + ' (' + vocaliPresenti + ')';
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
